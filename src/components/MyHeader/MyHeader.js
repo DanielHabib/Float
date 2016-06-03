2391f1366dd409e7b4dcf02b5a2839bf33d63364
Daniel Habib <d.g.habib7@gmail.com>
2016-06-03 12:57:47 -0400

    Change brand color

--- src/components/MyHeader/MyHeader.js
+++ src/components/MyHeader/MyHeader.js
@@ -36,7 +36,7 @@ class MyHeader extends Component {
     var style = {
 
     brand: {
-      color: "#5FAA9D",
+      color: "#FFF",
       "textDecoration": "none",
       fontSize: "1.75em",
       marginLeft: 20,
@@ -51,7 +51,7 @@ class MyHeader extends Component {
 
   link:{
     display: "inline-block",
-    color: "#5FAA9D",
+    color: "#FFF",
     padding: "3px 8px",
     textDecoration: "none",
     fontFamily: "Segoe UI, HelveticaNeue-Light, sans-serif",
